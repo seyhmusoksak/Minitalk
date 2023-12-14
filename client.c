476375ab26de74c943648f69f0734fd8ab14f70f
seyhmus <seyhmusoksak@gmail.com>
2023-12-15 01:11:08 +0300

    update

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: soksak <soksak@42istanbul.com.tr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 00:48:30 by soksak            #+#    #+#             */
-/*   Updated: 2023/12/12 14:15:54 by soksak           ###   ########.fr       */
+/*   Updated: 2023/12/15 01:06:39 by soksak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,28 @@
 
 int	ft_atoi(const char *str)
 {
-	int	i;
-	int	result;
+	int		i;
+	int		result;
+	int		sign;
 
 	i = 0;
+	sign = 1;
 	result = 0;
-	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	while ((str[i]) && (str[i] == '\n' || str[i] == '\t' || str[i] == '\f'
+			|| str[i] == '\v' || str[i] == '\r' || str[i] == ' '))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
 	{
-		result = result * 10 + str[i] - 48;
+		if (str[i] == '-')
+			sign *= -1;
 		i++;
 	}
-	return (result);
+	while (str[i] != '\0' && str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - 48);
+		i++;
+	}
+	return (result * sign);
 }
 
 void	pushsignal(int pid, char c)
